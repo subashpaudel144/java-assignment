b5910555e868431f7ca9c1bd1b8669afe12488ba
subashpaudel144 <60833698+subashpaudel144@users.noreply.github.com>
2020-02-09 21:03:33 +0545

    Update interface.java

--- assignment-1/interface.java
+++ assignment-1/interface.java
@@ -1,1 +1,25 @@
 
+
+interface Animal {
+    public void animalSound(); 
+    public void sleep(); 
+  }
+  
+  class Dog implements Animal {
+    public void animalSound() {
+      
+      System.out.println("The dog says: hou hou ");
+    }
+    public void sleep() {
+    
+      System.out.println("suiiiiiiii");
+    }
+  }
+  
+  class interface_in_java {
+    public static void main(String[] args) {
+      Dog mydog = new Dog(); 
+      mydig.animalSound();
+      myPig.sleep();
+    }
+  }
