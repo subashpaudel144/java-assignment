11dffa8fc02052907cbc982cc830e3de42337ef7
subashpaudel144 <60833698+subashpaudel144@users.noreply.github.com>
2020-03-04 17:58:05 +0545

    Add files via upload

---
+++ assignment3/Array.java
@@ -0,0 +1,8 @@
+class Array{
+    public static void main(String[] args){
+        String[]  sports = {"football" , "basketball" , "baseball" , "volleyball" , "cricket"};
+        for(int i = 0 ; i < sports.length ; i++){
+            System.out.println(sports[i]);
+        }
+    }
+}
---
+++ assignment3/EnhancedLopp.java
@@ -0,0 +1,9 @@
+class EnhancedLoop{
+    public static void main(String[] args){
+
+        char[] vowels = {'a' , 'e' , 'i' , 'o' , 'u'};
+        for( char i: vowels){
+            System.out.println(i);
+        }
+    }
+}
